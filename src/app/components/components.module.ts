9d7d60935ef2b8c6d9e255c914a66e6d35427735
Alexander <alexchavezlop@gmail.com>
2020-07-12 15:19:58 -0300

    First commit. Creating the project.

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DataTableComponent } from './data-table/data-table.component';
+
+@NgModule({
+  declarations: [ DataTableComponent ],
+  exports: [ DataTableComponent ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ComponentsModule { }
